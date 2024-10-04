8a07a6da5dc29443d9bfbe3136401018ebe2ead2
Diana Hogescu <hogescu_didii@yahoo.com>
2024-10-04 20:10:48 +0200

    small changes

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("#about_sublist").on("click", function () {
+    $("#about_sublist ul").slideToggle();
+  });
+  $("#adoption_sublist").on("click", function () {
+    $("#adoption_sublist li").slideToggle();
+  });
+
+
+});
